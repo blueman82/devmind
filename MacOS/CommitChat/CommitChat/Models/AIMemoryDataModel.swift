3cff51173c22c22a074d248920fa7cc102f3a697
blueman82 <>
2025-09-02 11:19:44 +0100

    Modify AIMemoryDataModel.swift • Tool: Edit • Timestamp: 2025-09-02 11:19:44

--- MacOS/CommitChat/CommitChat/Models/AIMemoryDataModel.swift
+++ MacOS/CommitChat/CommitChat/Models/AIMemoryDataModel.swift
@@ -309,7 +309,7 @@ class AIMemoryDataManager: ObservableObject, @unchecked Sendable {
     // MARK: - Indexing Methods
     
     func indexConversation(_ conversation: IndexableConversation) async throws {
-        try await Task.detached { [weak self] in
+        try Task { [weak self] in
             guard let self = self else {
                 throw AIMemoryError.databaseError("Database manager deallocated")
             }
